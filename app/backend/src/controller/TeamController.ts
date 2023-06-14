0d7bf6fa719dc3382c9063ace2aba269008e6aa1
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-14 20:00:29 -0300

    cria método handleGetTeams do controller de time

---
+++ app/backend/src/controller/TeamController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import ITeamService from '../Interfaces/teams/ITeamService';
+import HTTP from '../utils/HTTP';
+
+class TeamController {
+  private teamService: ITeamService;
+  public statusMapper = HTTP;
+
+  constructor(teamService: ITeamService) {
+    this.teamService = teamService;
+  }
+
+  public handleGetTeams = async (req: Request, res: Response) => {
+    const { status, data } = await this.teamService.getAll();
+
+    if (status !== 'SUCCESS') {
+      return res
+        .status(this.statusMapper.mapStatus(status))
+        .json(data);
+    }
+    res.status(200).json(data);
+  };
+}
+
+export default TeamController;
