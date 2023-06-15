e866c8e80c44637d95862f86a10720bc24074c1e
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-15 15:31:29 -0300

    cria método middleware de validação de token

--- app/backend/src/middleware/Validations.ts
+++ app/backend/src/middleware/Validations.ts
@@ -1,11 +1,35 @@
 import { NextFunction, Request, Response } from 'express';
+import ITokenManager from '../Interfaces/tokens/ITokenManager';
+import IUser from '../Interfaces/users/IUser';
 
 class Validations {
+  private tokenManager: ITokenManager;
+
+  constructor(tokenManager: ITokenManager) {
+    this.tokenManager = tokenManager;
+  }
+
   public static validateLogin = (req: Request, res: Response, next: NextFunction) => {
     const { email, password } = req.body;
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
+
+    next();
+  };
+
+  public validateToken = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.get('authorization');
+    if (!token) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+
+    const tokenValidation = this.tokenManager.validate<IUser>(token);
+    if (!tokenValidation.valid) {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
+
+    console.log(tokenValidation.data);
 
     next();
   };
