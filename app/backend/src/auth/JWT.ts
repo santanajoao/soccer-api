a16ea2eed177a4470bb0b19c4dab6e729decef70
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-06-15 15:30:38 -0300

    cria função validate da classe de JWT

--- app/backend/src/auth/JWT.ts
+++ app/backend/src/auth/JWT.ts
@@ -1,22 +1,32 @@
 import * as jwt from 'jsonwebtoken';
-import ITokenManager from '../Interfaces/ITokenManager';
+import TokenValidationResponse from '../Interfaces/tokens/TTokenValidationResponse';
+import ITokenManager from '../Interfaces/tokens/ITokenManager';
 
 class JWT implements ITokenManager {
-  private secret = process.env.JWT_SECRET || '';
   private manager = jwt;
 
-  private checkSecret = () => {
-    if (this.secret === '') {
-      throw new Error('Secret not found');
+  private static getSecret = (): string => {
+    const secret = process.env.JWT_SECRET;
+    if (!secret) {
+      throw new Error('JWT secret not found');
     }
+    return secret;
   };
 
   public generate = (payload: object): string => {
-    this.checkSecret();
-
-    const token = this.manager.sign(payload, this.secret);
+    const token = this.manager.sign(payload, JWT.getSecret());
     return token;
   };
+
+  public validate<T>(token: string): TokenValidationResponse<T> {
+    const secret = JWT.getSecret();
+    try {
+      const payload = this.manager.verify(token, secret) as T;
+      return { valid: true, data: payload };
+    } catch {
+      return { valid: false, data: null };
+    }
+  }
 }
 
 export default JWT;
